fa7c067cfde41c96dd95ae0a3821460ef13bdaab
Yong-Hae Lee <sundlee@gmail.com>
2020-08-20 15:34:14 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # myshoppingmall
+
+드림 코팅 페이지의 "쇼핑몰 미니게임 클론 코딩 & 코드 리뷰"수업 실습입니다.
+
+* 쇼핑몰 미니게임 클론 코딩: https://academy.dream-coding.com/courses/mini-shopping
---
+++ myshoppingmall.js
@@ -0,0 +1,51 @@
+/* fetch the items from the json file */
+function loadItems() {
+  return fetch('data/malldata.json')
+    .then((response) => response.json())
+    .then((json) => json.items)
+}
+
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  const html = items.map((item) => createHTMLString(item)).join('');
+  // console.log(html);
+  container.innerHTML = items.map((item) => createHTMLString(item)).join('');
+}
+
+function createHTMLString(item) {
+  return `
+      <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+      </li>
+  `;
+}
+
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  displayItems(items.filter((item) => item[key] === value));
+}
+
+function setEventListener(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', (event) => onButtonClick(event, items));
+}
+
+/* main */
+loadItems()
+  .then((items) => {
+    console.log(items);
+    displayItems(items);
+    setEventListener(items);
+  })
+  .catch(console.log);
